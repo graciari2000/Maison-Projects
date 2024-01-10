03b82d0a2a6dc58b6bace6089cf1c8235fcfa1ee
graciari2000 <92252649+graciari2000@users.noreply.github.com>
2024-01-10 13:17:38 +0000

    first php school project

---
+++ From-School/php/test_connection.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost"
+$username = "root"
+$password = ""
+$dbname = "test";
+
+//create connection
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+
+//check
+if ($connection) {
+    die("connection failed:" . mysqli_connect_error());    # code...
+}
+
+echo "Connected Successfully";
+
+mysqli_close($connection);
+?>
